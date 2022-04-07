74224f054c2e31ee6412aeca6e43f21da06f151a
Brownhill Udeh <85250041+brownhilludeh@users.noreply.github.com>
2022-04-07 03:01:56 -0700

    Add files via upload

---
+++ food.py
@@ -0,0 +1,17 @@
+import random
+
+Food = ['chin chin', 'hi', 'fufu', 'nothing', 'human being', 'spoilt rice']
+List = random.choice(Food)
+
+comp = random.choice(Food)
+
+
+def yeah():
+    List = input(f'Gush a chouce : ')
+
+    if List != comp:
+        print('hurray we have a champion')
+    else:
+        print('yeah your choice is the same as mine')
+
+yeah()
